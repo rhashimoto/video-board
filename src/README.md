2e0950b3a38aaa2680170aa10350a09362a08016
Roy Hashimoto <roy@shoestringresearch.com>
2022-06-11 09:17:58 -0700

    Add Firebase.

---
+++ src/README.md
@@ -0,0 +1,8 @@
+# video-board
+
+## firebase
+* Create new Firebase project
+* Add Web app to project
+* Enable Google authentication
+* Create Realtime Database
+  * https://github.com/firebase/firebase-js-sdk/issues/3707#issuecomment-708953179
--- src/video-board.js
+++ src/video-board.js
@@ -1,5 +1,27 @@
 import { LitElement, css, html } from 'lit';
 
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyASXeGyZygO7d_j_wfR6NiE-Fk49pG1uoQ",
+  authDomain: "shoestring-videoboard.firebaseapp.com",
+  projectId: "shoestring-videoboard",
+  storageBucket: "shoestring-videoboard.appspot.com",
+  messagingSenderId: "104957196093",
+  appId: "1:104957196093:web:806779aabe41c1fee07754",
+  measurementId: "G-V13R1N46M6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
 class VideoBoard extends LitElement {
   static properties = {
     example: { attribute: null }
