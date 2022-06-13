257568b7959e8dad2589854e53b30c1d8fef867d
Roy Hashimoto <roy@shoestringresearch.com>
2022-06-13 09:33:06 -0700

    Implement WebRTC connection.

--- src/video-board.js
+++ src/video-board.js
@@ -10,6 +10,43 @@ import { getAccessToken, getCredential } from './gapi.js';
 
 const MESSAGE_EXPIRATION_MILLIS = 60_000;
 const MEDIA_CONSTRAINTS = { audio: true, video: { facingMode: "user" } };
+const RTC_CONFIG = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302"
+    },
+    {
+      urls: "stun:stun1.l.google.com:19302"
+    },
+    {
+      urls: "stun:stun2.l.google.com:19302"
+    },
+    {
+      urls: "stun:stun3.l.google.com:19302"
+    },
+    {
+      urls: "stun:stun4.l.google.com:19302"
+    },
+    {
+      urls: "stun:openrelay.metered.ca:80",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ]
+};
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -39,8 +76,6 @@ const firebaseConfig = {
 });
 
 class VideoBoard extends LitElement {
-  instanceId = Math.random().toString(36).replace('0.', '');
-  clientUIDs = new Set();
 
   static properties = {
     example: { attribute: null }
@@ -64,39 +99,119 @@ class VideoBoard extends LitElement {
     this.uid = auth.currentUser.uid;
 
     const inbox = ref(this.database, `clients/${this.uid}/in`);
-    onChildAdded(inbox, snapshot => this.handleMessage(snapshot));
+    let inboxChain = Promise.resolve();
+    onChildAdded(inbox, snapshot => {
+      inboxChain = inboxChain.then(() => this.handleIncoming(snapshot));
+    });
   }
 
-  handleMessage(snapshot) {
+  async handleIncoming(snapshot) {
     const message = snapshot.val();
-    if (message.dst !== this.uid && message.dst !== this.instanceId) return;
+    console.log('inbox message', message);
     try {
       if (message.timestamp > Date.now() - MESSAGE_EXPIRATION_MILLIS) {
-        // TODO: dispatch message
-        console.log('message', message);
+        if (message.offer) {
+          const outbox = ref(this.database, `clients/${this.uid}/out`);
+
+          this.peerConnection = await this.#createPeerConnection();
+          this.peerConnection.onicecandidate = ({candidate}) => {
+            if (candidate) {
+              push(outbox, {
+                timestamp: Date.now(),
+                contextId: message.contextId,
+                candidate: candidate.toJSON()
+              });
+            }
+          };
+          this.peerConnection.ontrack = ({streams}) => {
+            const element = this.shadowRoot.getElementById('answerer');
+            element.srcObject = streams[0];
+          };
+            
+          await this.peerConnection.setRemoteDescription(message.offer);
+
+          const answer = await this.peerConnection.createAnswer();
+          await this.peerConnection.setLocalDescription(answer);
+          push(outbox, {
+            timestamp: Date.now(),
+            contextId: message.contextId,
+            answer: answer.toJSON()
+          });
+        } else if (message.candidate) {
+          this.peerConnection.addIceCandidate(message.candidate);
+        }
       }
     } finally {
       remove(snapshot.ref);
     }
   }
 
-  _call() {
-    // Post offer to destination inbox.
-    const dst = this.shadowRoot.getElementById('target').value;
-    const inbox = ref(this.database, `clients/${dst}/in`);
-    push(inbox, {
-      src: this.instanceId,
-      dst,
-      timestamp: Date.now(),
-      data: 'how now brown cow'
-    });
+  async #createPeerConnection() {
+    const peerConnection = new RTCPeerConnection(RTC_CONFIG);
 
-    // Listen for responses.
-    if (!this.clientUIDs.has(dst)) {
-      const outbox = ref(this.database, `clients/${dst}/out`);
-      onChildAdded(outbox, snapshot => this.handleMessage(snapshot));
-      this.clientUIDs.add(dst);
+    const mediaStream = await navigator.mediaDevices.getUserMedia(MEDIA_CONSTRAINTS);
+    for (const track of mediaStream.getTracks()) {
+      peerConnection.addTrack(track, mediaStream);
     }
+    return peerConnection;
+  }
+
+  async _call() {
+    const contextId = Math.random().toString(36).replace('0.', '');
+    const clientId = this.shadowRoot.getElementById('target').value;
+
+    const peerConnection = await this.#createPeerConnection();
+    peerConnection.onnegotiationneeded = async () => {
+      const offer = await peerConnection.createOffer();
+      await peerConnection.setLocalDescription(offer);
+
+      const inbox = ref(this.database, `clients/${clientId}/in`);
+      push(inbox, {
+        timestamp: Date.now(),
+        contextId,
+        offer: offer.toJSON()
+      })
+    };
+    peerConnection.onicecandidate = ({candidate}) => {
+      if (candidate) {
+        const inbox = ref(this.database, `clients/${clientId}/in`);
+        push(inbox, {
+          timestamp: Date.now(),
+          contextId,
+          candidate: candidate.toJSON()
+        });
+      }
+    };
+    peerConnection.ontrack = ({streams}) => {
+      const element = this.shadowRoot.getElementById('offerer');
+      element.srcObject = streams[0];
+    };
+    peerConnection.oniceconnectionstatechange = () => {
+      console.log('iceConnectionState', peerConnection.iceConnectionState);
+      if (peerConnection.iceConnectionState === 'disconnected') {
+        // TODO: tear down connection
+      }
+    };
+
+    // Listen for answer.
+    const outbox = ref(this.database, `clients/${clientId}/out`);
+    const closeSignaling = onChildAdded(outbox, async (snapshot) => {
+      const message = snapshot.val();
+      console.log('outbox message', message);
+      if (message.contextId === contextId) {
+        if (message.answer) {
+          await peerConnection.setRemoteDescription(message.answer);
+        } else if (message.candidate) {
+          peerConnection.addIceCandidate(message.candidate);
+        }
+      }
+      remove(snapshot.ref);
+    });
+    peerConnection.onconnectionstatechange = () => {
+      if (peerConnection.connectionState === 'connected') {
+        closeSignaling();
+      }
+    };
   }
 
   static get styles() {
@@ -110,8 +225,12 @@ class VideoBoard extends LitElement {
   render() {
     return html`
       <h1>Hello VideoBoard</h1>
-      <input id="target" value="rJKmGm9VjuXjmiTuLi9Wcj7TOtt2">
-      <button id="call" @click=${this._call}>Call</button>
+      <div>
+        <input id="target" value="rJKmGm9VjuXjmiTuLi9Wcj7TOtt2">
+        <button id="call" @click=${this._call}>Call</button>
+      </div>
+      <video id="offerer" autoplay width="160" height="120"></video>
+      <video id="answerer" autoplay width="160" height="120"></video>
     `;
   }
 }
