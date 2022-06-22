2f281fa6c36aacfeb85520588e66f6468c0d5485
Roy Hashimoto <roy@shoestringresearch.com>
2022-06-22 06:36:42 -0700

    Create client.html.

---
+++ src/client-top.js
@@ -0,0 +1,53 @@
+import { css, html, LitElement } from 'lit';
+
+// TODO: remove
+import './gapi.js';
+
+class ClientTop extends LitElement {
+  static properties = {
+    date: { attribute: null },
+    time: { attribute: null },
+  }
+
+  constructor() {
+    super();
+    this.#updateDateTime();
+  }
+
+  #updateDateTime() {
+    this.date = new Date().toLocaleDateString(undefined, { weekday: 'long', month: 'long', day: 'numeric' });
+    this.time = new Date().toLocaleTimeString(undefined, { timeStyle: 'short' });
+    setTimeout(() => this.#updateDateTime(), 1000);
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+      }
+
+      #bar {
+        display: flex;
+        justify-content: space-between;
+      }
+
+      #container {
+        flex: auto 1 1;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <div id="bar">
+        <span>${this.date}</span>
+        <span>${this.time}</span>
+      </div>
+      <div id="container"></div>
+    `;
+  }
+}
+customElements.define('client-top', ClientTop);
