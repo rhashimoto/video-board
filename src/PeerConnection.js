9db7748b9d397d5b276897cd150dc420913fd9b3
Roy Hashimoto <roy@shoestringresearch.com>
2022-07-01 15:37:24 -0700

    Add <client-rtc> and rtc-test.html.

--- src/PeerConnection.js
+++ src/PeerConnection.js
@@ -1,42 +1,126 @@
 export class PeerConnection extends RTCPeerConnection {
-  /** @type {Promise<RTCDataChannel>} */ #dataChannelReady;
+  #messageChannel = new MessageChannel();
+  #hasLocalMedia = false;
 
   /**
    * @param {RTCConfiguration} config 
+   * @param {{polite: boolean, remoteView: HTMLElement}} options
    */
-  constructor(config) {
+  constructor(config, options) {
     super(config);
 
-    this.#dataChannelReady = new Promise(resolve => {
-      const dataChannel = this.createDataChannel('chat', {
-        negotiated: true,
-        id: 0
-      });
-      dataChannel.addEventListener('open', () => {
-        resolve(dataChannel);
-      }, { once: true });
-      dataChannel.addEventListener('message', ({data}) => {
-        this.dispatchEvent(new CustomEvent('data', {
-          detail: data
-        }));
-      });
-    });
+    // Adapt to WebRTC perfect negotiation sample code.
+    // https://w3c.github.io/webrtc-pc/#perfect-negotiation-example
+    const pc = this;
+    /** @type {*} */ const signaling = this.#messageChannel.port1;
+    signaling.send = (message) => signaling.postMessage(JSON.stringify(message));
+    signaling.start();
+    const polite = options.polite;
+    const remoteView = /** @type {HTMLVideoElement} */ (options.remoteView ?? {});
+
+    pc.ontrack = ({track, streams}) => {
+      // once media for a remote track arrives, show it in the remote video element
+      track.onunmute = () => {
+        // don't set srcObject again if it is already set.
+        if (remoteView.srcObject) return;
+        remoteView.srcObject = streams[0];
+      };
+    };
+
+    // - The perfect negotiation logic, separated from the rest of the application ---
+    
+    // keep track of some negotiation state to prevent races and errors
+    let makingOffer = false;
+    let ignoreOffer = false;
+    let isSettingRemoteAnswerPending = false;
+    
+    // send any ice candidates to the other peer
+    pc.onicecandidate = ({candidate}) => signaling.send({candidate});
+    
+    // let the "negotiationneeded" event trigger offer generation
+    pc.onnegotiationneeded = async () => {
+      try {
+        makingOffer = true;
+        await pc.setLocalDescription();
+        signaling.send({description: pc.localDescription});
+      } catch (err) {
+         console.error(err);
+      } finally {
+        makingOffer = false;
+      }
+    };
+    
+    signaling.onmessage = async ({data: {description, candidate}}) => {
+      try {
+        if (description) {
+          // An offer may come in while we are busy processing SRD(answer).
+          // In this case, we will be in "stable" by the time the offer is processed
+          // so it is safe to chain it on our Operations Chain now.
+          const readyForOffer =
+              !makingOffer &&
+              (pc.signalingState == "stable" || isSettingRemoteAnswerPending);
+          const offerCollision = description.type == "offer" && !readyForOffer;
+    
+          ignoreOffer = !polite && offerCollision;
+          if (ignoreOffer) {
+            return;
+          }
+          isSettingRemoteAnswerPending = description.type == "answer";
+          await pc.setRemoteDescription(description); // SRD rolls back as needed
+          isSettingRemoteAnswerPending = false;
+          if (description.type == "offer") {
+            await pc.setLocalDescription();
+            signaling.send({description: pc.localDescription});
+          }
+        } else if (candidate) {
+          try {
+            await pc.addIceCandidate(candidate);
+          } catch (err) {
+            if (!ignoreOffer) throw err; // Suppress ignored offer's candidates
+          }
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    // this.#dataChannelReady = new Promise(resolve => {
+    //   const dataChannel = this.createDataChannel('chat', {
+    //     negotiated: true,
+    //     id: 0
+    //   });
+    //   dataChannel.addEventListener('open', () => {
+    //     resolve(dataChannel);
+    //   }, { once: true });
+    // });
+  }
+
+  /**
+   * @returns {MessagePort}
+   */
+  getSignalPort() {
+    return this.#messageChannel.port2;
   }
 
   /**
    * @param {MediaStream|MediaStreamConstraints} constraintsOrStream 
+   * @returns {Promise<MediaStream>}
    */
   async addMediaStream(constraintsOrStream) {
+    if (this.#hasLocalMedia) throw new Error('media already added');
+
     const mediaStream = constraintsOrStream instanceof MediaStream ?
       constraintsOrStream :
       await navigator.mediaDevices.getUserMedia(constraintsOrStream);
     for (const track of mediaStream.getTracks()) {
       this.addTrack(track, mediaStream);
     }
+
+    this.#hasLocalMedia = true;
+    return mediaStream;
   }
 
-  async send(data) {
-    const dataChannel = await this.#dataChannelReady;
-    dataChannel.send(data);
+  hasLocalMedia() {
+    return this.#hasLocalMedia;
   }
 }
---
+++ src/client-rtc.js
@@ -0,0 +1,178 @@
+import { css, html, LitElement } from 'lit';
+import { onChildAdded, push, ref, remove } from "firebase/database";
+import { repeat } from 'lit/directives/repeat.js';
+
+import { getFirebaseUid, getFirebaseDatabase } from './firebase.js';
+import { PeerConnection } from './PeerConnection.js';
+
+const MESSAGE_EXPIRATION_MILLIS = 60_000;
+
+const MEDIA_CONSTRAINTS = { audio: true, video: { facingMode: "user" } };
+const RTC_CONFIG = {
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
+    { urls: "stun:stun2.l.google.com:19302" },
+    { urls: "stun:stun3.l.google.com:19302" },
+    { urls: "stun:stun4.l.google.com:19302" },
+    { urls: "stun:openrelay.metered.ca:80" },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ]
+};
+
+class ClientRTC extends LitElement {
+  #ready;
+  #uid;
+  #database;
+  #runOnDisconnect = [];
+
+  // Call state.
+  /** @type {PeerConnection} */ #peerConnection = null;
+  #nonce = null;
+
+  static properties = {
+    peerId: { attribute: null },
+    peers: { attribute: null }
+  }
+
+  constructor() {
+    super();
+    this.#ready = this.#initialize();
+    this.peers = new Map();
+  }
+
+  async #initialize() {
+    this.#uid = await getFirebaseUid();
+    this.#database = await getFirebaseDatabase();
+
+    // Fetch peers.
+    const peers = ref(this.#database, `/users/${this.#uid}/peers`)
+    const unsubPeers = onChildAdded(peers, snapshot => {
+      this.peers = new Map(this.peers);
+      this.peers.set(snapshot.key, snapshot.val());
+    });
+    this.#runOnDisconnect.push(unsubPeers);
+
+    // Listen for signaling messages.
+    const incoming = ref(this.#database, `/users/${this.#uid}/inbox`);
+    const unsubInbox = onChildAdded(incoming, snapshot => {
+      const message = snapshot.val();
+      console.log('incoming', message);
+      if (message.timestamp > Date.now() - MESSAGE_EXPIRATION_MILLIS) {
+        this.#handleMessage(message);
+      }
+      remove(snapshot.ref);
+    });
+    this.#runOnDisconnect.push(unsubInbox);
+  }
+
+  async #handleMessage(message) {
+    if (this.#nonce !== null && this.#nonce !== message.nonce) {
+      // TODO: Tear down existing connection.
+    }
+    this.#nonce = message.nonce;
+
+    // Create the connection if necessary.
+    if (!this.#peerConnection) {
+      this.#peerConnection = this.#createPeerConnection(message.src);
+    }
+    this.#peerConnection.getSignalPort().postMessage(JSON.parse(message.data));
+  }
+
+  async #start() {
+    if (!this.#peerConnection) {
+      const peerSelector = this.shadowRoot.getElementById('peer-selector');
+      this.#peerConnection = this.#createPeerConnection(peerSelector['value']);
+    }
+
+    if (!this.#peerConnection.hasLocalMedia()) {
+      const mediaStream = await this.#peerConnection.addMediaStream(MEDIA_CONSTRAINTS);
+      const localView = /** @type {HTMLVideoElement} */
+        (this.shadowRoot.getElementById('local'));
+      if (!localView.srcObject) {
+        localView.srcObject = mediaStream;
+      }
+    }
+  }
+
+  #createPeerConnection(dst) {
+    const peerConnection = new PeerConnection(RTC_CONFIG, {
+      polite: this.#uid < dst,
+      remoteView: this.shadowRoot.getElementById('remote')
+    });
+
+    const port = peerConnection.getSignalPort()
+    port.addEventListener('message', ({data}) => this.#post(dst, data));
+    port.start();
+
+    return peerConnection;
+  }
+
+  async #post(dst, data) {
+    await this.#ready;
+
+    const message = {
+      timestamp: Date.now(),
+      nonce: this.#nonce || (this.#nonce = Math.random().toString(36).replace('0.', '')),
+      src: this.#uid,
+      data
+    };
+    await push(ref(this.#database, `/users/${dst}/inbox`), message);
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        gap: 1em;
+      }
+
+      video {
+        border: 1px solid;
+        width: 320px;
+        min-height: 160px;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <div>
+        <select id="peer-selector">
+          ${repeat(this.peers, ([uid, email]) => {
+            return html`
+              <option value="${uid}">${email}</option>
+            `
+          })}
+        </select>
+        <button @click=${this.#start}>Call</button>
+      </div>
+      <video id="local" autoplay muted></video>
+      <video id="remote" autoplay></video>
+    `;
+  }
+
+  disconnectedCallback() {
+    for (const fn of this.#runOnDisconnect) {
+      try {
+        fn();
+      } catch (e) {
+      }
+    }
+  }
+}
+customElements.define('client-rtc', ClientRTC);
